90fa80dac5436249cbc5de5d770f7a561dae3e2f
Jonathan Levin <JonathanWuzHere@gmail.com>
2016-07-10 16:19:50 +0300

    Add Climbing mechanic:
    1) Make IInteractable, IClimbable interfaces
    2) Make ClimbableTree (: IClimbable) script
    3) Duplicate and rename:
        * Platformer2DUserControl -> ScoutUserControl
        * PlatformerCharacter2D -> ScoutController
    
    4) ScoutUserControl - change so that script checks for vertical movement as well and calls ScoutUserControl.Move with vertical input (for climbing)
    
    5) ScoutController -
        * add OnTriggerEnter / Exit to check for climbable objects
        * add StartClimbing / StopClimbing methods
        * make Climb method to be called from Move()
        * Modularize Jump() function
        * add variable to control climb speed
        * make it so that climbing is disabled for a short time after jumping to be able to leapfrog up a vine
        * Probably a bunch of other stuff. I should do more commits instead of one big one so I remember next time
    
    6) Add layer - "Platforms"
    7) move floor tiles to Platform layer (apply to prefab)
    
    8) make "climbable pillar", add ClimbableTree script (for testing)

---
+++ Assets/Scripts/ClimbableTree.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbableTree : MonoBehaviour, IClimbable {
+    // A script for a tree that's climbable.
+    // the basic script for climbable GameObjects
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnActivate(GameObject climber)
+    {
+        return;
+    }
+
+    public void OnDeactivate(GameObject climber)
+    {
+        return;
+    }
+}
